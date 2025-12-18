aa8fc12a6544d258bf520461175d7c4172aeb361
ProxyBoy11 <ishan23vyas@gmail.com>
2025-12-18 20:26:30 +0530

    redeploy chat commit

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config(); // Load environment variables from .env file
+require("dotenv").config();
+const cors = require("cors");
+
 const userRoutes = require("./routes/user");
 const skillsRoutes = require("./routes/skills");
 const reviewRoutes = require("./routes/reviews");
 const conversationRoutes = require("./routes/conversation");
 const messageRoutes = require("./routes/message");
-const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(express.json());
 app.use(
   cors({
     origin: "*",
   })
 );
+
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/skills", skillsRoutes);
 app.use("/api/v1/reviews", reviewRoutes);
 app.use("/api/v1/conversation", conversationRoutes);
 app.use("/api/v1/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGO_URI_SKILLSWAP, {
@@ -30,10 +37,7 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB...");
-    app.listen(PORT, () => {
-      console.log(`Server listening at PORT ${PORT}...`);
-    });
   })
   .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+    console.error("MongoDB connection error:", err);
   });
